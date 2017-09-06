58a8ec4f5790d1bdf3cdabcdd7161c904d3746c1
Jiaxi Gu <imjiaxi@gmail.com>
2017-09-06 17:22:42 +0800

    fix

--- libmobi.rb
+++ libmobi.rb
@@ -8,9 +8,9 @@ class Libmobi < Formula
   bottle do
     root_url "https://github.com/gujiaxi/homebrew-iz/raw/master/bottles"
     cellar :any_skip_relocation
-    sha256 "5be571fbfbf68a3cba88a59b1e086bf5ac1f7120d36d3706097e877178dd3cf8" => :sierra
-    sha256 "5be571fbfbf68a3cba88a59b1e086bf5ac1f7120d36d3706097e877178dd3cf8" => :el_capitan
-    sha256 "5be571fbfbf68a3cba88a59b1e086bf5ac1f7120d36d3706097e877178dd3cf8" => :yosemite
+    sha256 "ef75907890114af4d9343bff7e496ed93cc774718f27ce2b47288a30e35187f0" => :sierra
+    sha256 "003978a483ab9abedb12b073ffb0b8603539d47efeb407059db026e294588e0a" => :el_capitan
+    sha256 "3fb38b9f0a97b7ab5248c46b3ba739077e908669eb72e8ec4e46062a7e2c8a91" => :yosemite
   end
 
   depends_on "automake" => :build
