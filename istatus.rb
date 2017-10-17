f57f61746fa398921af38d9d592f12ccd68d43ba
Jiaxi Gu <imjiaxi@gmail.com>
2017-10-17 14:43:48 +0800

    fix on high sierra for istatus

--- istatus.rb
+++ istatus.rb
@@ -1,9 +1,9 @@
 class Istatus < Formula
   desc "A gadget to show system information of macOS."
   homepage "https://github.com/gujiaxi/iStatus"
-  url "https://github.com/gujiaxi/iStatus/archive/v0.2.tar.gz"
-  version "0.2"
-  sha256 "ba2cfdb6ce9bc7c5469752583c714ab4c005ff359c97c6c28b21c5d83c4d3a80"
+  url "https://github.com/gujiaxi/iStatus/archive/v0.3.tar.gz"
+  version "0.3"
+  sha256 "ad6d9109595dff347420d578202e393b97573d97dc007761f7ff8e1836d479d0"
 
   def install
     system "make"
