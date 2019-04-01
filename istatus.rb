efa59753fa02067cc8ad0e80ae90cf27cecfc0d2
Jiaxi Gu <imjiaxi@gmail.com>
2019-04-01 18:15:27 +0800

    update description

--- istatus.rb
+++ istatus.rb
@@ -1,7 +1,7 @@
 class Istatus < Formula
-  desc "A gadget to show system information of macOS."
+  desc "A CLI tool to show system information of macOS."
   homepage "https://github.com/gujiaxi/iStatus"
-  url "https://github.com/gujiaxi/iStatus/archive/v0.4.tar.gz"
+  url "https://github.com/gujiaxi/iStatus/archive/v0.5.tar.gz"
   version "0.5"
   sha256 "1c22e9f2b4cde65e0a41c6a3bcdb958835a70ccadc8cb9c638f9c9cb626f3f52"
 
