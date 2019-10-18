b02a0e6b52eccc37d8f7c0a0d7fc9772d6a2909a
Jiaxi Gu <imjiaxi@gmail.com>
2019-10-18 12:18:26 +0800

    update vim from 8.1.1600 to 8.1.2150

--- Formula/vim.rb
+++ Formula/vim.rb
@@ -3,8 +3,8 @@ class Vim < Formula
   desc "Vi 'workalike' with many additional features"
   homepage "https://www.vim.org/"
   # vim should only be updated every 50 releases on multiples of 50
-  url "https://github.com/vim/vim/archive/v8.1.1600.tar.gz"
-  sha256 "4c73c88a164a74c57c7efad891e461edfc55d8dfe7f70b95c101abfe54eec3a4"
+  url "https://github.com/vim/vim/archive/v8.1.2150.tar.gz"
+  sha256 "0ab3a2656f3f2a4e9b9834ce2c119aaa6c895847f7df779f3aeaa3e87f974b75"
   head "https://github.com/vim/vim.git"
 
   LANGUAGES_OPTIONAL = %w[lua ruby perl].freeze
